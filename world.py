e590c6b087e61062769e1734acb8565cd8c00177
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2025-07-06 15:43:18 -0500

    add re_gen_passthrough for UT

--- world.py
+++ world.py
@@ -1,12 +1,26 @@
 from typing import ClassVar, Dict, Any, List
+from typing_extensions import override
+
+from Options import Option
 
 try:
     from rule_builder import RuleWorldMixin
 except ModuleNotFoundError:
     from .rule_builder import RuleWorldMixin
 from .options import GatorOptions, gator_options_presets, gator_option_groups
-from .items import item_name_to_id, item_table, item_name_groups, GatorItemName as I, GatorEventName as E
-from .locations import location_name_to_id, location_table, location_name_groups, GatorEventLocationName as EL
+from .items import (
+    item_name_to_id,
+    item_table,
+    item_name_groups,
+    GatorItemName as I,
+    GatorEventName as E,
+)
+from .locations import (
+    location_name_to_id,
+    location_table,
+    location_name_groups,
+    GatorEventLocationName as EL,
+)
 from .regions import GatorRegionName as R
 from .entrances import gator_entrances
 from .rules import Has, set_location_rules
@@ -21,8 +35,10 @@ gator_version = 102
 class GatorItem(Item):
     game: str = "Lil Gator Game"
 
+
 class GatorLocation(Location):
     game: str = "Lil Gator Game"
+
 
 class GatorWeb(WebWorld):
     theme = "jungle"
@@ -59,7 +75,6 @@ class GatorWorld(RuleWorldMixin, World):
     item_name_groups = item_name_groups
     location_name_groups = location_name_groups
 
-
     #  UT Integration
     tracker_world: ClassVar[dict[str, Any]] = tracker_world
     ut_can_gen_without_yaml: ClassVar[bool] = True
@@ -69,13 +84,32 @@ class GatorWorld(RuleWorldMixin, World):
     def interpret_slot_data(slot_data: Dict[str, Any]) -> Dict[str, Any]:
 
         # Adapted from ClassicSpeed's SADX implementation of this feature
-        if "APWorldVersion" in slot_data and slot_data["APWorldVersion"] != gator_version:
+        if (
+            "APWorldVersion" in slot_data
+            and slot_data["APWorldVersion"] != gator_version
+        ):
             current_version = f"v{gator_version // 100}.{(gator_version // 10) % 10}.{gator_version % 10}"
             slot_version = f"v{slot_data['APWorldVersion'] // 100}.{(slot_data['APWorldVersion'] // 10) % 10}.{slot_data['APWorldVersion'] % 10}"
 
             raise Exception(
-                f"Lil Gator Game version error: The version of apworld used to generate this world ({slot_version}) does not match the version of your installed apworld ({current_version}).")
+                f"Lil Gator Game version error: The version of apworld used to generate this world ({slot_version}) does not match the version of your installed apworld ({current_version})."
+            )
         return slot_data
+    
+    @override
+    def generate_early(self) -> None:
+        re_gen_passthrough = getattr(self.multiworld, "re_gen_passthrough", {})
+        if re_gen_passthrough and self.game in re_gen_passthrough:
+            # Get the passed through slot data from the real generation
+            slot_data: dict[str, Any] = re_gen_passthrough[self.game]
+
+            slot_options: dict[str, Any] = slot_data.get("options", {})
+            # Set all your options here instead of getting them from the yaml
+            for key, value in slot_options.items():
+                opt: Option[Any] | None = getattr(self.options, key, None)
+                if opt is not None:
+                    # You can also set .value directly but that won't work if you have OptionSets
+                    setattr(self.options, key, opt.from_any(value))
 
     ### Consider: having events for each playground construction
 
@@ -85,15 +119,19 @@ class GatorWorld(RuleWorldMixin, World):
             self.multiworld.regions.append(region)
 
         for gator_entrance in gator_entrances:
-            start_region = self.multiworld.get_region(gator_entrance.starting_region.value, self.player)
-            end_region = self.multiworld.get_region(gator_entrance.ending_region.value, self.player)
+            start_region = self.multiworld.get_region(
+                gator_entrance.starting_region.value, self.player
+            )
+            end_region = self.multiworld.get_region(
+                gator_entrance.ending_region.value, self.player
+            )
             self.create_entrance(start_region, end_region, gator_entrance.rule)
 
         for location_data in location_table:
-            region = self.multiworld.get_region(
-                location_data.region.value, self.player
+            region = self.multiworld.get_region(location_data.region.value, self.player)
+            location = GatorLocation(
+                self.player, location_data.name.value, location_data.location_id, region
             )
-            location = GatorLocation(self.player, location_data.name.value, location_data.location_id, region)
             region.locations.append(location)
 
         # Currently, only goal is complete the playground
@@ -108,7 +146,7 @@ class GatorWorld(RuleWorldMixin, World):
         # if the name provided is an event, create it as an event
         if name in [member.value for member in E]:
             return GatorItem(name, ItemClassification.progression, None, self.player)
-        
+
         # otherwise, look up the item data
         item_data = next(data for data in item_table if data.name.value == name)
         return GatorItem(
@@ -128,20 +166,19 @@ class GatorWorld(RuleWorldMixin, World):
         else:
             items_to_create[I.MEGAPHONE.value] = 1
             items_to_create[I.TEXTING.value] = 1
-        
+
         if self.options.lock_pots_behind_items:
             items_to_create[I.OAR.value] = 1
             items_to_create[I.TIGER_FORM.value] = 1
             items_to_create[I.GIANT_SOCKS.value] = 1
             items_to_create[I.SLEEP_MASK.value] = 1
             items_to_create[I.GUITAR.value] = 1
-        
+
         if self.options.lock_chests_behind_key:
             items_to_create[I.KEY.value] = 1
-        
+
         if self.options.lock_races_behind_flag:
             items_to_create[I.FINISH_FLAG.value] = 1
-        
 
         for item, quantity in items_to_create.items():
             for i in range(0, quantity):
@@ -164,7 +201,6 @@ class GatorWorld(RuleWorldMixin, World):
 
         # generate_rule_json()
 
-
     def fill_slot_data(self) -> Dict[str, Any]:
         # In order for our game client to handle the generated seed correctly we need to know what the user selected
         # for whether they should have access to Freeplay immediately.
@@ -172,10 +208,17 @@ class GatorWorld(RuleWorldMixin, World):
         # The options dataclass has a method to return a `Dict[str, Any]` of each option name provided and the relevant
         # option's value.
         slot_data = self.options.as_dict(
-            "start_with_freeplay", "require_shield_jump", "harder_ranged_quests", "lock_pots_behind_items", "lock_chests_behind_key", "lock_races_behind_flag"
+            "start_with_freeplay",
+            "require_shield_jump",
+            "harder_ranged_quests",
+            "lock_pots_behind_items",
+            "lock_chests_behind_key",
+            "lock_races_behind_flag",
         )
         slot_data["APWorldVersion"] = gator_version
         return slot_data
 
     def get_filler_item_name(self) -> str:
         return self.random.choice([I.CRAFT_15.value, I.CRAFT_30.value])
+
+
